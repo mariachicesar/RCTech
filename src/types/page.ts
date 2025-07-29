07008efb2a3d3c35afe33136ab1764b841a6b763
mariachicesar <cesar.rios@nakinc.org>
2025-07-29 10:59:17 -0700

    read pages done. Todo would be update

---
+++ src/types/page.ts
@@ -0,0 +1,35 @@
+export interface Page {
+  content: string | null;
+  created_at: string | null;
+  id: number;
+  slug: string | null;
+  title: string | null;
+  type: string | null;
+  website_id: number | null;
+}
+
+export interface FormData {
+  seoTitle: string;
+  seoKeywords: string;
+  seoDescription: string;
+  title: string;
+  slug: string;
+}
+
+export interface FormErrors {
+  seoTitle: string;
+  seoKeywords: string;
+  seoDescription: string;
+  title: string;
+  slug: string;
+}
+
+export interface SelectOption {
+  value: number;
+  label: string;
+}
+
+export interface ImageUploadLocation {
+  table: string;
+  id: number;
+}
